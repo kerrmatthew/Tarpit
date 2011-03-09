0ec07659d4bd084012f1969aef3cf50cb99693a9
Matthew Kerr <kerr.matthew@gmail.com>
2011-03-10 10:11:47 +1300

    update deletions

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, "Tarpit"
 set :repository,  "file:///Library/Git/tarpit.git"
 set :local_repository,  "matthewkerr@wud-web.com:/Library/Git/tarpit.git"
 
-set :deploy_to, "/Volumes/Macintosh HD2/alpha.tarpit/"
+set :deploy_to, "\"/Volumes/Macintosh HD2/alpha.tarpit/\""
 set :use_sudo, false
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
