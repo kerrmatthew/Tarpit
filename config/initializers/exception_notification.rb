2ddf95050f8736bf996c98f7c7b5f77eeba430b8
Matthew Kerr <kerr.matthew@gmail.com>
2011-04-01 15:25:56 +1300

    exception notification goes to a non gmail address

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -3,4 +3,4 @@ require 'exception_notifier'
 Tarpit::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "Tarpit",
     :sender_address => "tarpit_errors@tarpit_errors",
-    :exception_recipients => 'kerr.matthew@gmail.com'
+    :exception_recipients => 'matthew.kerr@me.com'
