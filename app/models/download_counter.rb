71270153818f63bd51b6905571cc3db2b83e1bde
Matthew Kerr <kerr.matthew@gmail.com>
2011-05-24 15:05:16 +1200

    download history styles

--- app/models/download_counter.rb
+++ app/models/download_counter.rb
@@ -1,10 +1,8 @@
 class DownloadCounter < ActiveRecord::Base
 
-validates_presence_of :user, :fossil
-
-belongs_to :user
-belongs_to :fossil
-
-
+  validates_presence_of :fossil
+  
+  belongs_to :user
+  belongs_to :fossil
 
 end
