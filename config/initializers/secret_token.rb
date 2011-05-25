e82ee35c077a31b73fb2dd2dfd57b35c6eadd711
Matthew Kerr <kerr.matthew@gmail.com>
2011-05-26 11:16:25 +1200

    bundle madness

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tarpit::Application.config.secret_token = 'd2ffcdb006385a3c0d04aff98e3d3bc123d85296d0c201b65f8673df42d9f1e4fdef6250a70f695bc8719420d884f4055cf4af056c4e8fcabe9419f09aab66d4'
+Tarpit::Application.config.secret_token = 'd2ffcdb006385a3c0d04aff98e3d3bc123d85296d0c201b65f8673df42d9f1e4fdef6250a70f695bc8719420d884f4055cf4af056c4e8fcabe9419f09aab66d3'
