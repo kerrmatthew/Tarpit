f4a376e2070ade95a030746c23d48755857fbd45
Matthew Kerr <kerr.matthew@gmail.com>
2011-06-08 01:33:39 +1200

    typekit works

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+//load typekit fonts
+try{Typekit.load();}catch(e){};
